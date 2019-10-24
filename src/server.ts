81712cc94349a1d7fa4ff1a098070548817adcc7
Florent Fauchille <florent.fauchille@bettermarks.com>
2019-10-24 19:45:37 +0200

    WIP

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { default as express, Request, Response } from "express";
+import {json} from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse our payload as `application/json`
+app.use(json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Hello, you're origin url is ${req.originalUrl}!`);
+});
+
+app.post("/course/checkin", (req: Request, res: Response) => {
+  if (req.body && req.body.name && req.body.group && req.body.slogan) {
+    res.send({ status: "inscrit!" });
+  } else {
+    res.status(400);
+    res.send({ status: "missing parameters or wrong parameters. Try again!" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API started on port ${port}`);
+});
